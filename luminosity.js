5b43d0a5da3a0291d2bb3e7757cca468ca041dc0
Michael Germany <mike@rickgermany.com>
2015-02-17 14:11:53 -0700

    finished part 2

--- luminosity.js
+++ luminosity.js
@@ -2,5 +2,8 @@ var luminosity = function() {
 	return 0.2126 * process.argv[2] + 0.7152 * process.argv[3] + 0.0722 * process.argv[4];
 };
 
-
+var darken = function () {
+	darkLuminosity = 0.2126 * (process.argv[2] * 0.8) + 0.7152 * (process.argv[2] * 0.8) + 0.0722 * (process.argv[2] * 0.8);
+	return darkLuminosity;
+};
 console.log(luminosity());
